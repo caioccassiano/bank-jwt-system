9476f14b4fc9686b422801941ab0befa1b8cd8c8
caioccassiano <caioccassiano@icloud.com>
2025-05-25 08:51:16 +0200

    feat: Implementing composers to the project

---
+++ src/main/composer/balance_editor_composer.py
@@ -0,0 +1,14 @@
+from src.models.repositories.user_repository import UserRepository
+from src.controllers.balance_editor import BalanceEditor
+from src.models.settings.db_connection_handler import db_connection_handler
+from src.views.balance_editor_view import BalanceEditorView
+
+def balance_editor_composer():
+  conn = db_connection_handler.get_connection()
+  model = UserRepository(conn=conn)
+  controller = BalanceEditor(user_repository=model)
+  view = BalanceEditorView(controller=controller)
+
+  return view
+
+
